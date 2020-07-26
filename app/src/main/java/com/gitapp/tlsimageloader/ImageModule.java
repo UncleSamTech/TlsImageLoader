be7c68cb53f41474501bb439785e9720cf6b5916
Ademar Alves de Oliveira <ademar111190@gmail.com>
2020-07-26 13:03:10 -0300

    Loading the image, no tls factory needed

---
+++ app/src/main/java/com/gitapp/tlsimageloader/ImageModule.java
@@ -0,0 +1,8 @@
+package com.gitapp.tlsimageloader;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class ImageModule extends AppGlideModule {
+}
